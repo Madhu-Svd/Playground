3ba951f014b53d4e1ff7134444fcff080878ac6e
madhu <madhucsvd@gmail.com>
2019-09-06 00:38:30 +0530

    updated android

--- android/ActivityLifeCycle/app/src/main/java/com/example/madhu/activitylifecycle/MainActivity.java
+++ android/ActivityLifeCycle/app/src/main/java/com/example/madhu/activitylifecycle/MainActivity.java
@@ -11,48 +11,48 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d("Activity LifeCycle","onCreate() method is invoked");
+        Log.d("Activity LifeCycle","onCreate() method was invoked");
     }
     @Override
     protected void onStart() {
 
         super.onStart();
-        Log.d("Activity LifeCycle","onStart() method is called");
+        Log.d("Activity LifeCycle","onStart() method was called");
 
     }
     @Override
     protected void onResume() {
 
         super.onResume();
-        Log.d("Activity LifeCycle","onResume() method is called");
+        Log.d("Activity LifeCycle","onResume() method was called");
 
     }
     @Override
     protected void onPause() {
 
         super.onPause();
-        Log.d("Activity LifeCycle","onPause() method is called");
+        Log.d("Activity LifeCycle","onPause() method was called");
 
     }
     @Override
     protected void onStop() {
 
         super.onStop();
-        Log.d("Activity LifeCycle","onStop() method is called");
+        Log.d("Activity LifeCycle","onStop() method was called");
 
     }
     @Override
     protected void onRestart() {
 
         super.onRestart();
-        Log.d("Activity LifeCycle","onStop() method is called");
+        Log.d("Activity LifeCycle","onRestart() method was called");
 
     }
     @Override
     protected void onDestroy() {
 
         super.onDestroy();
-        Log.d("Activity LifeCycle","onStop() method is called");
+        Log.d("Activity LifeCycle","onDestroy() method was called");
 
     }
 }
