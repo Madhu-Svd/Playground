b84badee0250868ec55e9ab4c692d7e5607a7d47
madhu <madhucsvd@gmail.com>
2019-09-07 09:45:46 +0530

    updated android
    Constructor Activity

--- android/ConstructorActivity/app/src/main/java/com/example/madhu/constructoractivity/ConstructorDemo.java
+++ android/ConstructorActivity/app/src/main/java/com/example/madhu/constructoractivity/ConstructorDemo.java
@@ -2,9 +2,15 @@ package com.example.madhu.constructoractivity;
 
 import android.widget.Toast;
 
-public class ConstructorDemo extends MainActivity {
+public class ConstructorDemo extends MainActivity{
     public ConstructorDemo(){
-//            System.out.println("dkjdhjdbhjgdh");
-            Toast.makeText(getApplicationContext(),"This is a Constructor Demo example",Toast.LENGTH_LONG).show();
+        System.out.println("This is a Constructor Demo program");
+        System.out.println("Madhu");
+        System.out.println("CSE");
+        System.out.println("Constructor");
+        System.out.println("ConstructorDemo");
+
+
+//            Toast.makeText(getApplicationContext(),"constructor",Toast.LENGTH_LONG).show();
     }
 }
