184f3e513dfa30ca9cf92a5242bd265aef3834dc
madhu <madhucsvd@gmail.com>
2019-09-09 15:05:26 +0530

    Android
    Constructor Activity

--- android/ConstructorActivity/app/src/main/java/com/example/madhu/constructoractivity/ConstructorDemo.java
+++ android/ConstructorActivity/app/src/main/java/com/example/madhu/constructoractivity/ConstructorDemo.java
@@ -1,16 +1,31 @@
 package com.example.madhu.constructoractivity;
 
+import android.app.AppComponentFactory;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
 
-public class ConstructorDemo extends MainActivity{
-    public ConstructorDemo(){
-        System.out.println("This is a Constructor Demo program");
-        System.out.println("Madhu");
-        System.out.println("CSE");
-        System.out.println("Constructor");
-        System.out.println("ConstructorDemo");
-
-
-//            Toast.makeText(getApplicationContext(),"constructor",Toast.LENGTH_LONG).show();
+public class ConstructorDemo extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
     }
-}
+    public ConstructorDemo() {
+            System.out.println("This is a Constructor Demo program");
+            System.out.println("Madhu");
+            System.out.println("CSE");
+            System.out.println("Constructor");
+            System.out.println("ConstructorDemo");
+
+//            Toast.makeText(getApplicationContext(), "constructor", Toast.LENGTH_LONG).show();
+
+        }
+
+        public void display(Context applicationContext) {
+            Toast.makeText(applicationContext, "this is constructor", Toast.LENGTH_LONG).show();
+        }
+
+    }
+
--- android/ConstructorActivity/app/src/main/java/com/example/madhu/constructoractivity/MainActivity.java
+++ android/ConstructorActivity/app/src/main/java/com/example/madhu/constructoractivity/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ConstructorDemo constructorDemo=new ConstructorDemo();
+        constructorDemo.display(getApplicationContext());
     }
 }
