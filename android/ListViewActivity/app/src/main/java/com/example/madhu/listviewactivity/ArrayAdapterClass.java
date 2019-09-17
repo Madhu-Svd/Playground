ec624b7b4dcd814bc4dd3f1291cadf47fbb3069c
madhu <madhucsvd@gmail.com>
2019-09-17 17:26:35 +0530

    Android ListVeiw updated

--- android/ListViewActivity/app/src/main/java/com/example/madhu/listviewactivity/ArrayAdapterClass.java
+++ android/ListViewActivity/app/src/main/java/com/example/madhu/listviewactivity/ArrayAdapterClass.java
@@ -1,14 +1,57 @@
 package com.example.madhu.listviewactivity;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
-class ArrayAdapterClass {
-    public ArrayAdapterClass(Context applicationContext, ArrayList title, ArrayList status, ArrayList dateofapproval) {
+class ArrayAdapterClass extends BaseAdapter{
+    ArrayList<String> title_text;
+    ArrayList<String> status_text;
+    ArrayList<String> approvaldate;
+    private Context context;
+    public ArrayAdapterClass(Context Context, ArrayList<String> title, ArrayList<String> status, ArrayList<String> dateofapproval) {
+        this.title_text=title;
+        this.status_text=status;
+        this.approvaldate=dateofapproval;
+        this.context=Context;
     }
 
+    @Override
+    public int getCount() {
+        return title_text.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View view=LayoutInflater.from(context).inflate(R.layout.itemlist,parent,false);
+        TextView title,status,dateapproval;
+        ImageView imageView;
+
+        title=view.findViewById(R.id.it_title);
+        dateapproval=view.findViewById(R.id.it_date);
+        status=view.findViewById(R.id.it_status);
+        imageView=view.findViewById(R.id.imageView7);
+
+        title.setText( title_text.get(position));
+        status.setText(status_text.get(position));
+        dateapproval.setText(approvaldate.get(position));
+        imageView.setImageResource(R.drawable.bell_icon);
+        return view;
+    }
 }
--- android/ListViewActivity/app/src/main/java/com/example/madhu/listviewactivity/MainActivity.java
+++ android/ListViewActivity/app/src/main/java/com/example/madhu/listviewactivity/MainActivity.java
@@ -9,19 +9,14 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     ListView listView;
-    ArrayList title=new ArrayList<String>();
-    ArrayList status=new ArrayList<String>();
-    ArrayList dateofapproval=new ArrayList<String>();
-//    String title[]={"PUR-2019-056","PUR-2019-057","PUR-2019-058","PUR-2019-059","PUR-2019-60"};
-//    String status[]={"APPROVED","APPROVED","APPROVED","APPROVED","APPROVED"};
-//    String dateofapproval[]={"20-05-2019","21-05-2019","22-05-2019","23-05-2019","24-05-2019"};
+    ArrayList<String> title=new ArrayList<>();
+    ArrayList<String> status=new ArrayList<>();
+    ArrayList<String> dateofapproval=new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         listView=findViewById(R.id.listviewid);
-        ArrayAdapterClass adapter=new ArrayAdapterClass(getApplicationContext(),title,status,dateofapproval);
-        listView.setAdapter(adapter);
 
             title.add("PUR-2019-056");
             title.add("PUR-2019-057");
@@ -37,6 +32,8 @@ public class MainActivity extends AppCompatActivity {
             dateofapproval.add("22-05-2019");
             dateofapproval.add("23-05-2019");
             dateofapproval.add("24-05-2019");
+        ArrayAdapterClass adapter=new ArrayAdapterClass(getApplicationContext(),title,status,dateofapproval);
+        listView.setAdapter(adapter);
 
 
 
